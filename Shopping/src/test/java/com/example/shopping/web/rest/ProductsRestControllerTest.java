2c6cd57a0594b5c0674f997fb798e3bc1f94a1ec
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-02 16:18:51 +0200

    Fixing test case

--- Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
+++ Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
@@ -21,13 +21,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProductsRestControllerTest {
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@MockBean
-	private ProductService productService;
+    @MockBean
+    private ProductService productService;
 
-	@Test
+    @Test
     public void testGetAllProducts() throws Exception {
         when(productService.getAllProducts())
                 .thenReturn(List.of(createProduct("ACER", 1500), createProduct("ASUS", 2000)));
@@ -36,15 +36,11 @@ public class ProductsRestControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[0].productName", is("ACER")))
                 .andExpect(jsonPath("$.[1].productName", is("ASUS")))
-                .andExpect(jsonPath("$.[0].id", is(1)))
-                .andExpect(jsonPath("$.[1].id", is(1)))
                 .andExpect(jsonPath("$.[0].price", is(1500)))
                 .andExpect(jsonPath("$.[1].price", is(2000)));
     }
 
-	private ProductViewDto createProduct(String name, int price) {
-		ProductViewDto product = new ProductViewDto(name, BigDecimal.valueOf(price), "path", "url", 1);
-
-		return product;
-	}
+    private ProductViewDto createProduct(String name, int price) {
+        return new ProductViewDto(name, BigDecimal.valueOf(price), "path", "url", 1);
+    }
 }
