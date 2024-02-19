b990887524c2fcd8f6a06c55a5b855d888c9b5af
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-19 15:47:57 +0200

    Big fix

--- Shopping/src/main/java/com/example/shopping/model/entity/ProductEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/ProductEntity.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,7 +39,7 @@ public class ProductEntity {
 	@Column(name = "video_url")
 	private String videoUrl;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
 	private CategoryEntity category;
 
 	@OneToMany(targetEntity = SpecificationsEntity.class, mappedBy = "product")
--- Shopping/src/main/java/com/example/shopping/service/InitService.java
+++ Shopping/src/main/java/com/example/shopping/service/InitService.java
@@ -1,14 +1,5 @@
 package com.example.shopping.service;
 
-import com.example.shopping.model.entity.ProductEntity;
-import com.example.shopping.model.entity.SpecificationsEntity;
-import com.example.shopping.repository.OrderItemRepository;
-import com.example.shopping.repository.ProductRepository;
-import com.example.shopping.repository.ShoppingItemRepository;
-import com.example.shopping.repository.SpecificationsRepository;
-import com.google.gson.Gson;
-import org.springframework.stereotype.Service;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -16,53 +7,74 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.example.shopping.model.entity.CategoryEntity;
+import com.example.shopping.model.entity.ProductEntity;
+import com.example.shopping.model.entity.SpecificationsEntity;
+import com.example.shopping.model.enums.Category;
+import com.example.shopping.repository.CategoryRepository;
+import com.example.shopping.repository.OrderItemRepository;
+import com.example.shopping.repository.ProductRepository;
+import com.example.shopping.repository.ShoppingItemRepository;
+import com.example.shopping.repository.SpecificationsRepository;
+import com.google.gson.Gson;
+
 @Service
 public class InitService {
-    private final ProductRepository productRepository;
-    private final SpecificationsRepository specificationsRepository;
-    private final Gson gson;
-    private final ShoppingItemRepository shoppingItemRepository;
-    private final OrderItemRepository orderItemRepository;
+	private final ProductRepository productRepository;
+	private final SpecificationsRepository specificationsRepository;
+	private final Gson gson;
+	private final ShoppingItemRepository shoppingItemRepository;
+	private final OrderItemRepository orderItemRepository;
+	private final CategoryRepository categoryRepository;
 
-    public InitService(ProductRepository productRepository, Gson gson,
-                       SpecificationsRepository specificationsRepository, ShoppingItemRepository shoppingItemRepository,
-                       OrderItemRepository orderItemRepository) {
-        this.productRepository = productRepository;
-        this.specificationsRepository = specificationsRepository;
-        this.gson = gson;
-        this.shoppingItemRepository = shoppingItemRepository;
-        this.orderItemRepository = orderItemRepository;
-    }
+	public InitService(ProductRepository productRepository, Gson gson,
+			SpecificationsRepository specificationsRepository, ShoppingItemRepository shoppingItemRepository,
+			OrderItemRepository orderItemRepository, CategoryRepository categoryRepository) {
+		this.productRepository = productRepository;
+		this.specificationsRepository = specificationsRepository;
+		this.gson = gson;
+		this.shoppingItemRepository = shoppingItemRepository;
+		this.orderItemRepository = orderItemRepository;
+		this.categoryRepository = categoryRepository;
+	}
 
-    public void initDb() {
-        try (FileReader reader = new FileReader(
-                Path.of("src", "main", "resources", "static", "js", "products.json").toFile())) {
-            List<ProductEntity> products = Arrays.stream(gson.fromJson(reader, ProductEntity[].class)).toList();
-            List<SpecificationsEntity> specs = new ArrayList<>();
+	public void initDb() {
+		if (categoryRepository.count() == 0) {
+			for (Category c : Category.values()) {
+				this.categoryRepository.save(new CategoryEntity(c));
+			}
+		}
 
-            for (ProductEntity product : products) {
-                specs.addAll(product.getSpecs());
-            }
+		try (FileReader reader = new FileReader(
+				Path.of("src", "main", "resources", "static", "js", "products.json").toFile())) {
+			List<ProductEntity> products = Arrays.stream(gson.fromJson(reader, ProductEntity[].class)).toList();
+			List<SpecificationsEntity> specs = new ArrayList<>();
 
-            reader.close();
+			for (ProductEntity product : products) {
+				specs.addAll(product.getSpecs());
+			}
 
-            if (productRepository.count() != products.size() || specificationsRepository.count() != specs.size()) {
-                this.orderItemRepository.deleteAll();
-                this.shoppingItemRepository.deleteAll();
-                this.specificationsRepository.deleteAll();
-                this.productRepository.deleteAll();
+			reader.close();
 
-                products = this.productRepository.saveAll(products);
+			if (productRepository.count() != products.size() || specificationsRepository.count() != specs.size()) {
+				this.orderItemRepository.deleteAll();
+				this.shoppingItemRepository.deleteAll();
+				this.specificationsRepository.deleteAll();
+				this.productRepository.deleteAll();
 
-                for (ProductEntity product : products) {
-                    product.getSpecs().forEach(sp -> {
-                        sp.setProduct(product);
-                        this.specificationsRepository.save(sp);
-                    });
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("File doesn't exist!");
-        }
-    }
+				products = this.productRepository.saveAll(products);
+
+				for (ProductEntity product : products) {
+					product.getSpecs().forEach(sp -> {
+						sp.setProduct(product);
+						this.specificationsRepository.save(sp);
+					});
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("File doesn't exist!");
+		}
+	}
 }
