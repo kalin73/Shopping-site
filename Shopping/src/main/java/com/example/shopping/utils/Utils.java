7aea49353a9b5bae80e6a8f8a134311d468c5458
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-20 18:19:33 +0200

    Adding global order object

--- Shopping/src/main/java/com/example/shopping/utils/Utils.java
+++ Shopping/src/main/java/com/example/shopping/utils/Utils.java
@@ -1,5 +1,9 @@
 package com.example.shopping.utils;
 
+import com.example.shopping.model.dto.OrderDto;
+
 public class Utils {
     public static Boolean IS_VALID = true;
+
+    public static OrderDto currentOrder;
 }
