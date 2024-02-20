6c4bbfeb677a3b14c7ffb44a9219401efda862f0
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-02-20 18:26:43 +0200

    Adding endpoint for placing orders

--- Shopping/src/main/java/com/example/shopping/web/OrderController.java
+++ Shopping/src/main/java/com/example/shopping/web/OrderController.java
@@ -1,27 +1,57 @@
 package com.example.shopping.web;
 
+import com.example.shopping.model.dto.ApplicationUserDetails;
+import com.example.shopping.model.dto.OrderDto;
+import com.example.shopping.model.enums.PaymentMethod;
+import com.example.shopping.service.OrderService;
+import com.example.shopping.service.ShoppingCartService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.shopping.model.dto.ApplicationUserDetails;
-import com.example.shopping.service.ShoppingCartService;
+import static com.example.shopping.utils.Utils.currentOrder;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
-	private final ShoppingCartService shoppingCartService;
+    private final ShoppingCartService shoppingCartService;
 
-	public OrderController(ShoppingCartService shoppingCartService) {
-		this.shoppingCartService = shoppingCartService;
-	}
+    private final OrderService orderService;
 
-	@GetMapping("/checkout")
-	public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails user, ModelAndView modelAndView) {
-		this.shoppingCartService.loadShoppingCart(modelAndView, user);
-		modelAndView.setViewName("cardInformation");
+    public OrderController(ShoppingCartService shoppingCartService, OrderService orderService) {
+        this.shoppingCartService = shoppingCartService;
+        this.orderService = orderService;
+    }
 
-		return modelAndView;
-	}
+    @GetMapping("/checkout")
+    public String placeOrder() {
+
+        return "orderCheckout";
+    }
+
+    @PostMapping("/placeOrder")
+    public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails client,
+                                   OrderDto orderDto,
+                                   ModelAndView modelAndView) {
+        currentOrder = orderDto;
+
+        if (orderDto.getPaymentMethod().equals(PaymentMethod.CARD.name())) {
+            this.shoppingCartService.loadShoppingCart(modelAndView, client);
+            modelAndView.setViewName("cardInformation");
+
+            return modelAndView;
+        }
+
+        this.orderService.placeOrder(orderDto, client);
+
+        currentOrder = null;
+
+        modelAndView.setViewName("redirect:/");
+
+        return modelAndView;
+    }
 
 }
