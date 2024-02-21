b7d2882c22fc9345ddc58cb7011f5e0079f96752
Strahil Dimitrov <hristovstrahil5@gmail.com>
2024-02-21 12:16:21 +0200

    Update OrderController.java

--- Shopping/src/main/java/com/example/shopping/web/OrderController.java
+++ Shopping/src/main/java/com/example/shopping/web/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,7 +35,7 @@ public class OrderController {
 
     @PostMapping("/placeOrder")
     public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails client,
-                                   OrderDto orderDto,
+                                  @RequestBody OrderDto orderDto,
                                    ModelAndView modelAndView) {
         currentOrder = orderDto;
 
