3052240ca21b9a0137d261c549088ebed8bf9337
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-03-01 11:41:28 +0200

    commit

--- Shopping/src/main/java/com/example/shopping/service/InitService.java
+++ Shopping/src/main/java/com/example/shopping/service/InitService.java
@@ -1,5 +1,13 @@
 package com.example.shopping.service;
 
+import com.example.shopping.model.entity.CategoryEntity;
+import com.example.shopping.model.entity.ProductEntity;
+import com.example.shopping.model.entity.SpecificationsEntity;
+import com.example.shopping.model.enums.Category;
+import com.example.shopping.repository.*;
+import com.google.gson.Gson;
+import org.springframework.stereotype.Service;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -7,74 +15,62 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-import com.example.shopping.model.entity.CategoryEntity;
-import com.example.shopping.model.entity.ProductEntity;
-import com.example.shopping.model.entity.SpecificationsEntity;
-import com.example.shopping.model.enums.Category;
-import com.example.shopping.repository.CategoryRepository;
-import com.example.shopping.repository.OrderItemRepository;
-import com.example.shopping.repository.ProductRepository;
-import com.example.shopping.repository.ShoppingItemRepository;
-import com.example.shopping.repository.SpecificationsRepository;
-import com.google.gson.Gson;
-
 @Service
 public class InitService {
-	private final ProductRepository productRepository;
-	private final SpecificationsRepository specificationsRepository;
-	private final Gson gson;
-	private final ShoppingItemRepository shoppingItemRepository;
-	private final OrderItemRepository orderItemRepository;
-	private final CategoryRepository categoryRepository;
+    private static final String[] PRODUCTS_JSON_PATH = {"src", "main", "resources", "static", "js", "products.json"};
+    private final ProductRepository productRepository;
+    private final SpecificationsRepository specificationsRepository;
+    private final Gson gson;
+    private final ShoppingItemRepository shoppingItemRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final CategoryRepository categoryRepository;
 
-	public InitService(ProductRepository productRepository, Gson gson,
-			SpecificationsRepository specificationsRepository, ShoppingItemRepository shoppingItemRepository,
-			OrderItemRepository orderItemRepository, CategoryRepository categoryRepository) {
-		this.productRepository = productRepository;
-		this.specificationsRepository = specificationsRepository;
-		this.gson = gson;
-		this.shoppingItemRepository = shoppingItemRepository;
-		this.orderItemRepository = orderItemRepository;
-		this.categoryRepository = categoryRepository;
-	}
+    public InitService(ProductRepository productRepository, Gson gson,
+                       SpecificationsRepository specificationsRepository, ShoppingItemRepository shoppingItemRepository,
+                       OrderItemRepository orderItemRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.specificationsRepository = specificationsRepository;
+        this.gson = gson;
+        this.shoppingItemRepository = shoppingItemRepository;
+        this.orderItemRepository = orderItemRepository;
+        this.categoryRepository = categoryRepository;
+    }
 
-	public void initDb() {
-		if (categoryRepository.count() == 0) {
-			for (Category c : Category.values()) {
-				this.categoryRepository.save(new CategoryEntity(c));
-			}
-		}
+    public void initDb() {
+        if (categoryRepository.count() == 0) {
+            for (Category c : Category.values()) {
+                this.categoryRepository.save(new CategoryEntity(c));
+            }
+        }
 
-		try (FileReader reader = new FileReader(
-				Path.of("src", "main", "resources", "static", "js", "products.json").toFile())) {
-			List<ProductEntity> products = Arrays.stream(gson.fromJson(reader, ProductEntity[].class)).toList();
-			List<SpecificationsEntity> specs = new ArrayList<>();
+        try (FileReader reader = new FileReader(
+                Path.of("Shopping", PRODUCTS_JSON_PATH).toFile())) {
+            List<ProductEntity> products = Arrays.stream(gson.fromJson(reader, ProductEntity[].class)).toList();
+            List<SpecificationsEntity> specs = new ArrayList<>();
 
-			for (ProductEntity product : products) {
-				specs.addAll(product.getSpecs());
-			}
+            for (ProductEntity product : products) {
+                specs.addAll(product.getSpecs());
+            }
 
-			reader.close();
+            reader.close();
 
-			if (productRepository.count() != products.size() || specificationsRepository.count() != specs.size()) {
-				this.orderItemRepository.deleteAll();
-				this.shoppingItemRepository.deleteAll();
-				this.specificationsRepository.deleteAll();
-				this.productRepository.deleteAll();
+            if (productRepository.count() != products.size() || specificationsRepository.count() != specs.size()) {
+                this.orderItemRepository.deleteAll();
+                this.shoppingItemRepository.deleteAll();
+                this.specificationsRepository.deleteAll();
+                this.productRepository.deleteAll();
 
-				products = this.productRepository.saveAll(products);
+                products = this.productRepository.saveAll(products);
 
-				for (ProductEntity product : products) {
-					product.getSpecs().forEach(sp -> {
-						sp.setProduct(product);
-						this.specificationsRepository.save(sp);
-					});
-				}
-			}
-		} catch (IOException e) {
-			System.out.println("File doesn't exist!");
-		}
-	}
+                for (ProductEntity product : products) {
+                    product.getSpecs().forEach(sp -> {
+                        sp.setProduct(product);
+                        this.specificationsRepository.save(sp);
+                    });
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("File doesn't exist!");
+        }
+    }
 }
