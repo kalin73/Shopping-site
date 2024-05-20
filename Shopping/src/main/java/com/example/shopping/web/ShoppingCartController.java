bdcd004ddcb5734f6b0e599f670713249b8d07b8
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-20 18:54:04 +0300

    changing redirect

--- Shopping/src/main/java/com/example/shopping/web/ShoppingCartController.java
+++ Shopping/src/main/java/com/example/shopping/web/ShoppingCartController.java
@@ -8,48 +8,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import static com.example.shopping.utils.Utils.lastUrl;
+
 @Controller
 public class ShoppingCartController {
-	private final ShoppingCartService shoppingCartService;
+    private final ShoppingCartService shoppingCartService;
 
-	public ShoppingCartController(ShoppingCartService shoppingCartService) {
-		this.shoppingCartService = shoppingCartService;
-	}
+    public ShoppingCartController(ShoppingCartService shoppingCartService) {
+        this.shoppingCartService = shoppingCartService;
+    }
 
-	@GetMapping("/add/{id}")
-	public ModelAndView addItem(@PathVariable("id") Long id, @AuthenticationPrincipal ApplicationUserDetails user,
-			ModelAndView modelAndView) {
-		Long categoryId = 0L;
+    @GetMapping("/add/{id}")
+    public ModelAndView addItem(@PathVariable("id") Long id, @AuthenticationPrincipal ApplicationUserDetails user,
+                                ModelAndView modelAndView) {
+        if (user != null) {
+            this.shoppingCartService.addToCart(id, user.getUsername());
+        }
 
-		if (user != null) {
-			categoryId = this.shoppingCartService.addToCart(id, user.getUsername());
-		}
+        modelAndView.setViewName("redirect:" + lastUrl);
 
-		modelAndView.setViewName("redirect:/products/" + categoryId);
-		
-		return modelAndView;
+        return modelAndView;
 
-	}
+    }
 
-	@GetMapping("/deleteCart")
-	public ModelAndView deleteCart(@AuthenticationPrincipal ApplicationUserDetails user, ModelAndView modelAndView) {
-		if (user != null) {
-			this.shoppingCartService.deleteCart(user.getUsername());
-		}
+    @GetMapping("/deleteCart")
+    public ModelAndView deleteCart(@AuthenticationPrincipal ApplicationUserDetails user, ModelAndView modelAndView) {
+        if (user != null) {
+            this.shoppingCartService.deleteCart(user.getUsername());
+        }
 
-		modelAndView.setViewName("redirect:/");
+        modelAndView.setViewName("redirect:" + lastUrl);
 
-		return modelAndView;
+        return modelAndView;
 
-	}
+    }
 
-	@GetMapping("/delete/{id}")
-	public ModelAndView deleteItemById(@PathVariable("id") Long id, ModelAndView modelAndView) {
-		this.shoppingCartService.removeItemFromCart(id);
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteItemById(@PathVariable("id") Long id, ModelAndView modelAndView) {
+        this.shoppingCartService.removeItemFromCart(id);
 
-		modelAndView.setViewName("redirect:/");
+        modelAndView.setViewName("redirect:" + lastUrl);
 
-		return modelAndView;
+        return modelAndView;
 
-	}
+    }
 }
