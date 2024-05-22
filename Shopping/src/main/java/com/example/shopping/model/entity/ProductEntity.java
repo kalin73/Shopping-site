2c858f44f2c72bef27ac74c96df8d62bdbf4f5ea
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-23 00:46:54 +0300

    adding join column annotation

--- Shopping/src/main/java/com/example/shopping/model/entity/ProductEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/ProductEntity.java
@@ -4,143 +4,136 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "products")
 public class ProductEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "product_name", nullable = false)
-	private String productName;
+    @Column(name = "product_name", nullable = false)
+    private String productName;
 
-	@Column(nullable = false)
-	private BigDecimal price;
+    @Column(nullable = false)
+    private BigDecimal price;
 
-	@Column
-	private String description;
+    @Column
+    private String description;
 
-	@Column(nullable = false)
-	private int quantity;
+    @Column(nullable = false)
+    private int quantity;
 
-	@Column
-	private String image;
+    @Column
+    private String image;
 
-	@Column(name = "video_url")
-	private String videoUrl;
+    @Column(name = "video_url")
+    private String videoUrl;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	private CategoryEntity category;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private CategoryEntity category;
 
-	@OneToMany(targetEntity = SpecificationsEntity.class, mappedBy = "product")
-	private List<SpecificationsEntity> specs;
+    @OneToMany(targetEntity = SpecificationsEntity.class, mappedBy = "product")
+    private List<SpecificationsEntity> specs;
 
-	public ProductEntity() {
-		specs = new ArrayList<>();
-	}
+    public ProductEntity() {
+        specs = new ArrayList<>();
+    }
 
-	public ProductEntity(String productName, BigDecimal price, String description, int quantity, String image,
-			String videoUrl, CategoryEntity category, List<SpecificationsEntity> specs) {
-		this();
-		this.productName = productName;
-		this.price = price;
-		this.description = description;
-		this.quantity = quantity;
-		this.image = image;
-		this.videoUrl = videoUrl;
-		this.category = category;
-		this.specs = specs;
-	}
+    public ProductEntity(String productName, BigDecimal price, String description, int quantity, String image,
+                         String videoUrl, CategoryEntity category, List<SpecificationsEntity> specs) {
+        this();
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+        this.image = image;
+        this.videoUrl = videoUrl;
+        this.category = category;
+        this.specs = specs;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public ProductEntity setId(Long id) {
-		this.id = id;
-		return this;
-	}
+    public ProductEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
 
-	public String getProductName() {
-		return productName;
-	}
+    public String getProductName() {
+        return productName;
+    }
 
-	public ProductEntity setProductName(String productName) {
-		this.productName = productName;
-		return this;
-	}
+    public ProductEntity setProductName(String productName) {
+        this.productName = productName;
+        return this;
+    }
 
-	public BigDecimal getPrice() {
-		return price;
-	}
+    public BigDecimal getPrice() {
+        return price;
+    }
 
-	public ProductEntity setPrice(BigDecimal price) {
-		this.price = price;
-		return this;
-	}
+    public ProductEntity setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	public ProductEntity setQuantity(int quantity) {
-		this.quantity = quantity;
-		return this;
-	}
+    public ProductEntity setQuantity(int quantity) {
+        this.quantity = quantity;
+        return this;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public ProductEntity setDescription(String description) {
-		this.description = description;
-		return this;
-	}
+    public ProductEntity setDescription(String description) {
+        this.description = description;
+        return this;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public ProductEntity setImage(String image) {
-		this.image = image;
-		return this;
-	}
+    public ProductEntity setImage(String image) {
+        this.image = image;
+        return this;
+    }
 
-	public String getVideoUrl() {
-		return videoUrl;
-	}
+    public String getVideoUrl() {
+        return videoUrl;
+    }
 
-	public ProductEntity setVideoUrl(String videoUrl) {
-		this.videoUrl = videoUrl;
-		return this;
-	}
+    public ProductEntity setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+        return this;
+    }
 
-	public CategoryEntity getCategory() {
-		return category;
-	}
+    public CategoryEntity getCategory() {
+        return category;
+    }
 
-	public ProductEntity setCategory(CategoryEntity category) {
-		this.category = category;
-		return this;
-	}
+    public ProductEntity setCategory(CategoryEntity category) {
+        this.category = category;
+        return this;
+    }
 
-	public List<SpecificationsEntity> getSpecs() {
-		return specs;
-	}
+    public List<SpecificationsEntity> getSpecs() {
+        return specs;
+    }
 
-	public ProductEntity setSpecs(List<SpecificationsEntity> specs) {
-		this.specs = specs;
-		return this;
-	}
+    public ProductEntity setSpecs(List<SpecificationsEntity> specs) {
+        this.specs = specs;
+        return this;
+    }
 
 }
