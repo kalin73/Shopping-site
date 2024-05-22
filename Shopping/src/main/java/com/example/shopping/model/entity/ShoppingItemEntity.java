2867d01b7a0d5f8c9e7e8265ce991c645c3d6e6f
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-23 00:51:36 +0300

    adding join column annotation

--- Shopping/src/main/java/com/example/shopping/model/entity/ShoppingItemEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/ShoppingItemEntity.java
@@ -1,12 +1,6 @@
 package com.example.shopping.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "shopping_cart_items")
@@ -16,9 +10,11 @@ public class ShoppingItemEntity {
 	private Long id;
 
 	@ManyToOne
+	@JoinColumn(name = "product_id")
 	private ProductEntity product;
 
 	@ManyToOne
+	@JoinColumn(name = "cart_id")
 	private ShoppingCartEntity cart;
 
 	@Column
