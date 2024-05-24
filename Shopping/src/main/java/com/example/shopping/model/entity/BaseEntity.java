a4a6a7eb491f8a05b3126b980a31a39695e0d83c
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-05-24 16:36:54 +0300

    adding abstract entity class

--- Shopping/src/main/java/com/example/shopping/model/entity/BaseEntity.java
+++ Shopping/src/main/java/com/example/shopping/model/entity/BaseEntity.java
@@ -10,4 +10,13 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BaseEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
 }
