248ae15a687fde25275af86bb573d4b447b3b536
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-06-10 18:06:14 +0300

    creating phone number validator

---
+++ Shopping/src/main/java/com/example/shopping/validation/PhoneNumberValidator.java
@@ -0,0 +1,14 @@
+package com.example.shopping.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class PhoneNumberValidator implements ConstraintValidator<ValidatePhoneNumber, String> {
+
+    @Override
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        return Pattern.matches("^[0-9]{10}$", phoneNumber);
+    }
+}
