206641eb5b2e47e7ca5ba9e441a2f50c7efa8f90
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-09-14 00:17:17 +0300

    fixing tests

--- Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
+++ Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
@@ -1,49 +1,43 @@
 package com.example.shopping.web.rest;
 
-import com.example.shopping.model.dto.ProductViewDto;
-import com.example.shopping.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class ProductsRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private ProductService productService;
-
     @Test
     public void testGetAllProducts() throws Exception {
-        when(productService.getAllProducts())
-                .thenReturn(List.of(createProduct("ACER", 1500), createProduct("ASUS", 2000)));
-
         mockMvc.perform(get("/api/products")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].productName", is("ACER")))
-                .andExpect(jsonPath("$.[1].productName", is("ASUS")))
-                .andExpect(jsonPath("$.[0].price", is(1500)))
-                .andExpect(jsonPath("$.[1].price", is(2000)));
+                .andExpect(jsonPath("$.[0].productName", is("ASUS VIVOBOOK")))
+                .andExpect(jsonPath("$.[1].productName", is("Acer NITRO 5")))
+                .andExpect(jsonPath("$.[0].price", is(2000.00)))
+                .andExpect(jsonPath("$.[1].price", is(1299.00)));
     }
 
-    private ProductViewDto createProduct(String name, int price) {
-        return new ProductViewDto(name, BigDecimal.valueOf(price), "path", "url", 1);
+    @Test
+    public void testGetProductById() throws Exception {
+        mockMvc.perform(get("/api/product/info/{id}", 1)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productName").value("ASUS VIVOBOOK"))
+                .andExpect(jsonPath("$.price").value(2000.00))
+                .andExpect(jsonPath("$.description").value("Good laptop"));
+
     }
 }
