87f2e3ba93675b06023571398c1a51102841b8ae
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-11-14 18:37:28 +0200

    improving search query

--- Shopping/src/main/java/com/example/shopping/repository/ProductRepository.java
+++ Shopping/src/main/java/com/example/shopping/repository/ProductRepository.java
@@ -1,19 +1,20 @@
 package com.example.shopping.repository;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.example.shopping.model.entity.CategoryEntity;
+import com.example.shopping.model.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-
-import com.example.shopping.model.entity.CategoryEntity;
-import com.example.shopping.model.entity.ProductEntity;
 import org.springframework.transaction.annotation.Transactional;
 
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+import java.util.List;
+import java.util.Optional;
 
-    @Query("SELECT p FROM ProductEntity p WHERE p.productName LIKE %:filter%")
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    String SEARCH_QUERY = "SELECT p FROM ProductEntity p WHERE LOWER(p.productName) LIKE LOWER(CONCAT('%', :filter, '%'))" +
+            "OR LOWER(p.category.name) LIKE LOWER(CONCAT('%', :filter, '%'))";
+
+    @Query(SEARCH_QUERY)
     Optional<List<ProductEntity>> search(String filter);
 
     Optional<List<ProductEntity>> findAllByCategory(CategoryEntity category);
