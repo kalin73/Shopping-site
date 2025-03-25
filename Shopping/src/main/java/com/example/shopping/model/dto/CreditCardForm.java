080d3d43c8c7702b919d8212d66500a38d08a423
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-03-25 17:44:13 +0200

    adding validations

--- Shopping/src/main/java/com/example/shopping/model/dto/CreditCardForm.java
+++ Shopping/src/main/java/com/example/shopping/model/dto/CreditCardForm.java
@@ -1,14 +1,21 @@
 package com.example.shopping.model.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+
 public class CreditCardForm {
+    @Pattern(regexp = "[a-zA-Z]{2,20}\\s[a-zA-Z]{2,20}", message = "Invalid name format")
     private String ownerName;
 
+    @Pattern(regexp = "\\d{12}", message = "The card number should be exactly 12 digits")
     private String cardNumber;
 
+    @Pattern(regexp = "\\d{3}", message = "The cvc code should be exactly 3 digits")
     private String cvcCode;
 
     private String month;
 
+    @Min(2025)
     private int year;
 
     public String getOwnerName() {
