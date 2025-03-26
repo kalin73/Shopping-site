6f764a82fc745652241b250b15b82b4030294526
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-03-26 20:35:56 +0200

    validating input

--- Shopping/src/main/java/com/example/shopping/web/OrderController.java
+++ Shopping/src/main/java/com/example/shopping/web/OrderController.java
@@ -7,11 +7,16 @@ import com.example.shopping.service.OrderService;
 import com.example.shopping.service.ShoppingCartService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import static com.example.shopping.utils.Constants.BINDING_RESULT_PATH;
 import static com.example.shopping.utils.Utils.currentOrder;
 
 @Controller
@@ -34,24 +39,40 @@ public class OrderController {
 
     @PostMapping("/placeOrder")
     public ModelAndView placeOrder(@AuthenticationPrincipal ApplicationUserDetails client,
-                                   OrderDto orderDto,
+                                   @Validated OrderDto order,
+                                   BindingResult bindingResult,
+                                   RedirectAttributes redirectAttributes,
                                    ModelAndView modelAndView) {
-        currentOrder = orderDto;
+        currentOrder = order;
 
-        if (orderDto.getPaymentMethod().equals(PaymentMethod.CARD.name())) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("order", order)
+                    .addFlashAttribute(BINDING_RESULT_PATH + "order", bindingResult);
+
+            modelAndView.setViewName("redirect:/order/checkout");
+
+            return modelAndView;
+        }
+
+        if (order.getPaymentMethod().equals(PaymentMethod.CARD.name())) {
             this.shoppingCartService.loadShoppingCart(modelAndView, client);
             modelAndView.setViewName("cardInformation");
 
             return modelAndView;
         }
 
-        this.orderService.placeOrder(orderDto, client);
+        this.orderService.placeOrder(order, client);
 
         currentOrder = null;
 
         modelAndView.setViewName("redirect:/");
 
         return modelAndView;
+    }
+
+    @ModelAttribute(name = "order")
+    public OrderDto order() {
+        return new OrderDto();
     }
 
 }
