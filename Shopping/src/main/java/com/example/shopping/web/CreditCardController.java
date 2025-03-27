c6b1753cf1e6992c817cd6a8eea207a3292b6a93
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-03-27 16:54:18 +0200

    validating card info

--- Shopping/src/main/java/com/example/shopping/web/CreditCardController.java
+++ Shopping/src/main/java/com/example/shopping/web/CreditCardController.java
@@ -6,11 +6,18 @@ import com.example.shopping.service.CreditCardService;
 import com.example.shopping.service.OrderService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import static com.example.shopping.utils.Constants.BINDING_RESULT_PATH;
 import static com.example.shopping.utils.Utils.currentOrder;
 
+// TODO Verify credit card info
 @Controller
 @RequestMapping("/creditCard")
 public class CreditCardController {
@@ -22,19 +29,36 @@ public class CreditCardController {
         this.orderService = orderService;
     }
 
+    @GetMapping("/cardInformation")
+    public String cardInformation() {
+        return "cardInformation";
+    }
+
     @PostMapping("/add")
-    public String addCreditCard(CreditCardForm creditCardForm, @AuthenticationPrincipal ApplicationUserDetails userDetails) {
-        addCard(creditCardForm, userDetails.getUsername());
+    public String addCreditCard(@Validated CreditCardForm creditCardForm,
+                                BindingResult bindingResult,
+                                @AuthenticationPrincipal ApplicationUserDetails userDetails) {
+
+        this.creditCardService.addCreditCard(creditCardForm, userDetails.getUsername());
 
         return "redirect:/";
     }
 
     @PostMapping("/payWithCard")
     public String payWithCard(boolean save,
-                              CreditCardForm creditCardForm,
+                              @Validated CreditCardForm creditCardForm,
+                              BindingResult bindingResult,
+                              RedirectAttributes redirectAttributes,
                               @AuthenticationPrincipal ApplicationUserDetails user) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute(BINDING_RESULT_PATH + "creditCardForm", bindingResult)
+                    .addFlashAttribute("creditCardForm", creditCardForm);
+
+            return "redirect:/creditCard/cardInformation";
+        }
+
         if (save) {
-            addCard(creditCardForm, user.getUsername());
+            this.creditCardService.addCreditCard(creditCardForm, user.getUsername());
         }
 
         this.orderService.placeOrder(currentOrder, user);
@@ -44,7 +68,9 @@ public class CreditCardController {
         return "redirect:/";
     }
 
-    private void addCard(CreditCardForm creditCardForm, String username) {
-        this.creditCardService.addCreditCard(creditCardForm, username);
+    @ModelAttribute(name = "creditCardForm")
+    public CreditCardForm creditCardForm() {
+        return new CreditCardForm();
     }
+
 }
