994028e44d3dc89ad243b08532f733545b9c4809
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-03-31 13:17:16 +0300

    adding status code change for wrong input

--- Shopping/src/main/java/com/example/shopping/web/AddressController.java
+++ Shopping/src/main/java/com/example/shopping/web/AddressController.java
@@ -3,6 +3,7 @@ package com.example.shopping.web;
 import com.example.shopping.model.dto.AddressFormDto;
 import com.example.shopping.model.dto.ApplicationUserDetails;
 import com.example.shopping.service.AddressService;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -10,11 +11,12 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import static com.example.shopping.utils.Constants.BINDING_RESULT_PATH;
 
 @Controller
 public class AddressController {
-    private static final String BINDING_RESULT_PATH = "org.springframework.validation.BindingResult.";
     private final AddressService addressService;
 
     public AddressController(AddressService addressService) {
@@ -27,21 +29,24 @@ public class AddressController {
     }
 
     @PostMapping("/billingAddress")
-    public String addOrderAddress(@Validated AddressFormDto addressFrom,
-                                  BindingResult bindingResult,
-                                  RedirectAttributes redirectAttributes,
-                                  @AuthenticationPrincipal ApplicationUserDetails user) {
+    public ModelAndView addOrderAddress(@Validated AddressFormDto addressFrom,
+                                        BindingResult bindingResult,
+                                        @AuthenticationPrincipal ApplicationUserDetails user,
+                                        ModelAndView modelAndView) {
 
         if (bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("addressForm", addressFrom).
-                    addFlashAttribute(BINDING_RESULT_PATH + "addressFrom", bindingResult);
+            modelAndView.addObject("addressForm", addressFrom);
+            modelAndView.addObject(BINDING_RESULT_PATH + "addressForm", bindingResult);
+            modelAndView.setStatus(HttpStatus.BAD_REQUEST);
+            modelAndView.setViewName("addressForm");
 
-            return "redirect:/billingAddress";
+            return modelAndView;
         }
 
         this.addressService.addAddress(addressFrom, user.getUsername());
+        modelAndView.setViewName("redirect:/");
 
-        return "redirect:/";
+        return modelAndView;
     }
 
     @ModelAttribute(name = "addressForm")
