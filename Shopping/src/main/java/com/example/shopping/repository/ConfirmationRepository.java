a3b97a7999f953e7dc02f4a34c07ad31a6e51924
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-04-29 21:00:33 +0300

    adding method for getting confirmations by user

--- Shopping/src/main/java/com/example/shopping/repository/ConfirmationRepository.java
+++ Shopping/src/main/java/com/example/shopping/repository/ConfirmationRepository.java
@@ -1,8 +1,12 @@
 package com.example.shopping.repository;
 
 import com.example.shopping.model.entity.ConfirmationEntity;
+import com.example.shopping.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ConfirmationRepository extends JpaRepository<ConfirmationEntity, Long> {
     ConfirmationEntity findByToken(String token);
+    List<ConfirmationEntity> findAllByUser(UserEntity user);
 }
