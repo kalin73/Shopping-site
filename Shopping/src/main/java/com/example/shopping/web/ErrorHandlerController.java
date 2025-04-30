ddaa3b11e15fbf57b04132071225b03879189d04
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-04-30 16:22:32 +0300

    updating exception handling

--- Shopping/src/main/java/com/example/shopping/web/ErrorHandlerController.java
+++ Shopping/src/main/java/com/example/shopping/web/ErrorHandlerController.java
@@ -1,13 +1,19 @@
 package com.example.shopping.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ErrorHandlerController {
 
     @ExceptionHandler(Exception.class)
-    public String handleException(){
-        return "error";
+    public ModelAndView handleException() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+        modelAndView.setViewName("error");
+
+        return modelAndView;
     }
 }
