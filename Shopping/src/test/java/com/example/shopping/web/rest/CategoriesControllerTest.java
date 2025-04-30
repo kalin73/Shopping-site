f4c95603898e587e47496642b2d0a8e9a03b8937
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-04-30 16:06:05 +0300

    adding test for categories controller 100% coverage

---
+++ Shopping/src/test/java/com/example/shopping/web/rest/CategoriesControllerTest.java
@@ -0,0 +1,23 @@
+package com.example.shopping.web.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CategoriesControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetCategoriesPage() throws Exception {
+        mockMvc.perform(get("/categories"))
+                .andExpect(status().isOk());
+    }
+}
