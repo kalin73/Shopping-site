23209c4613f9edb79ef7f4b68bb250b23dcb4e76
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-05-08 15:55:51 +0300

    fixing test bug

--- Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
+++ Shopping/src/test/java/com/example/shopping/web/rest/ProductsRestControllerTest.java
@@ -5,6 +5,7 @@ import com.example.shopping.model.entity.ProductEntity;
 import com.example.shopping.model.enums.Category;
 import com.example.shopping.repository.CategoryRepository;
 import com.example.shopping.repository.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class ProductsRestControllerTest {
+    private static Long product1Id;
+    private static Long product2Id;
+    private static Long catId;
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -37,27 +42,32 @@ public class ProductsRestControllerTest {
     public void setUp() {
         CategoryEntity laptopCategory = new CategoryEntity();
         laptopCategory.setName(Category.LAPTOPS);
-        laptopCategory.setId(1L);
-        if (this.categoryRepository.count() == 0) this.categoryRepository.save(laptopCategory);
+        if (this.categoryRepository.count() == 0) {
+            catId = this.categoryRepository.save(laptopCategory).getId();
+        }
 
         ProductEntity product1 = new ProductEntity();
         product1.setProductName("ASUS VIVOBOOK")
                 .setPrice(BigDecimal.valueOf(2000))
                 .setDescription("Good laptop")
                 .setQuantity(1)
-                .setCategory(laptopCategory)
-                .setId(1L);
+                .setCategory(laptopCategory);
 
         ProductEntity product2 = new ProductEntity();
         product2.setProductName("Acer NITRO 5")
                 .setPrice(BigDecimal.valueOf(1299))
                 .setDescription("Good laptop")
                 .setQuantity(2)
-                .setCategory(laptopCategory)
-                .setId(2L);
+                .setCategory(laptopCategory);
 
-        this.productRepository.save(product1);
-        this.productRepository.save(product2);
+        product1Id = this.productRepository.save(product1).getId();
+        product2Id = this.productRepository.save(product2).getId();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.productRepository.deleteAll();
+        this.categoryRepository.deleteAll();
     }
 
     @Test
@@ -75,18 +85,25 @@ public class ProductsRestControllerTest {
 
     @Test
     public void testGetProductById() throws Exception {
-        mockMvc.perform(get("/api/product/info/{id}", 1)
+        mockMvc.perform(get("/api/product/info/{id}", product1Id)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.productName").value("ASUS VIVOBOOK"))
                 .andExpect(jsonPath("$.price").value(2000.00))
                 .andExpect(jsonPath("$.description").value("Good laptop"));
 
+        mockMvc.perform(get("/api/product/info/{id}", product2Id)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productName").value("Acer NITRO 5"))
+                .andExpect(jsonPath("$.price").value(1299.00))
+                .andExpect(jsonPath("$.description").value("Good laptop"));
+
     }
 
     @Test
     public void testGetProductsByCategory() throws Exception {
-        mockMvc.perform(get("/api/products/{catId}", 1)
+        mockMvc.perform(get("/api/products/{catId}", catId)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[0].productName", is("ASUS VIVOBOOK")))
