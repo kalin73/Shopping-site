3b18f4072c745ad2af8882727d2fa360b3cad8f5
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-05-19 19:59:56 +0300

    adding tests with 100% coverage

--- Shopping/src/test/java/com/example/shopping/web/CreditCardControllerTest.java
+++ Shopping/src/test/java/com/example/shopping/web/CreditCardControllerTest.java
@@ -1,10 +1,11 @@
 package com.example.shopping.web;
 
 import com.example.shopping.model.dto.RegisterFormDto;
+import com.example.shopping.model.entity.CategoryEntity;
+import com.example.shopping.model.entity.ProductEntity;
 import com.example.shopping.model.entity.UserEntity;
-import com.example.shopping.repository.ConfirmationRepository;
-import com.example.shopping.repository.CreditCardRepository;
-import com.example.shopping.repository.UserRepository;
+import com.example.shopping.model.enums.Category;
+import com.example.shopping.repository.*;
 import com.example.shopping.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,17 +16,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class CreditCardControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -41,6 +46,24 @@ public class CreditCardControllerTest {
 
     @Autowired
     private ConfirmationRepository confirmationRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private ShoppingItemRepository shoppingItemRepository;
+
+    @Autowired
+    private ShoppingCartRepository shoppingCartRepository;
 
     @BeforeEach
     public void setUp() {
@@ -61,9 +84,14 @@ public class CreditCardControllerTest {
 
     @AfterEach
     public void tearDown() {
-        this.creditCardRepository.deleteAll();
-        this.confirmationRepository.deleteAll();
-        this.userRepository.deleteAll();
+        confirmationRepository.deleteAll();
+        orderItemRepository.deleteAll();
+        orderRepository.deleteAll();
+        shoppingItemRepository.deleteAll();
+        shoppingCartRepository.deleteAll();
+        productRepository.deleteAll();
+        creditCardRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
@@ -114,6 +142,95 @@ public class CreditCardControllerTest {
 
         assertEquals(0, creditCardRepository.count());
 
+    }
+
+    @Test
+    @WithUserDetails(setupBefore = TestExecutionEvent.TEST_EXECUTION, value = "test@abv.bg")
+    public void testPayWithCardWhenLoggedIn() throws Exception {
+        assertEquals(0, orderRepository.count());
+
+        placeOrder();
+
+        mockMvc.perform(post("/creditCard/payWithCard")
+                        .params(getCreditCardParams()))
+                .andExpect(status().is3xxRedirection());
+
+        assertEquals(1, orderRepository.count());
+    }
+
+    @Test
+    public void testPayWithCardWithoutLoggedUser() throws Exception {
+        assertEquals(0, orderRepository.count());
+
+        placeOrder();
+
+        mockMvc.perform(post("/creditCard/payWithCard")
+                        .params(getCreditCardParams()))
+                .andExpect(status().is3xxRedirection());
+
+        assertEquals(0, orderRepository.count());
+    }
+
+    @Test
+    @WithUserDetails(setupBefore = TestExecutionEvent.TEST_EXECUTION, value = "test@abv.bg")
+    public void testPayWithCardWithWrongData() throws Exception {
+        assertEquals(0, orderRepository.count());
+
+        placeOrder();
+
+        mockMvc.perform(post("/creditCard/payWithCard")
+                        .param("ownerName", "Test Testov")
+                        .param("cardNumber", "1234 5678 9012")
+                        .param("cvcCode", "1")
+                        .param("month", "01")
+                        .param("year", "2028"))
+                .andExpect(status().isBadRequest())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrors("creditCardForm", "cardNumber"))
+                .andExpect(model().attributeHasFieldErrors("creditCardForm", "cvcCode"));
+
+        assertEquals(0, orderRepository.count());
+
+    }
+
+    @Test
+    @WithUserDetails(setupBefore = TestExecutionEvent.TEST_EXECUTION, value = "test@abv.bg")
+    public void testPayAndSaveCard() throws Exception {
+        assertEquals(0, orderRepository.count());
+        assertEquals(0, creditCardRepository.count());
+        placeOrder();
+
+        mockMvc.perform(post("/creditCard/payWithCard")
+                        .params(getCreditCardParams())
+                        .param("save", "true"))
+                .andExpect(status().is3xxRedirection());
+
+        assertEquals(1, orderRepository.count());
+        assertEquals(1, creditCardRepository.count());
+    }
+
+    private void placeOrder() throws Exception {
+        addToCart();
+        mockMvc.perform(post("/order/placeOrder")
+                .param("fullName", "Test Testov")
+                .param("phoneNumber", "0987654321")
+                .param("shippingAddress", "Test city")
+                .param("paymentMethod", "CARD"));
+    }
+
+    private void addToCart() throws Exception {
+        CategoryEntity category = new CategoryEntity(Category.COMPUTERS);
+        category = categoryRepository.save(category);
+
+        ProductEntity product = new ProductEntity();
+        product.setProductName("PC")
+                .setPrice(BigDecimal.valueOf(100000))
+                .setQuantity(3)
+                .setCategory(category);
+        product = productRepository.save(product);
+
+        //Adding product to shopping cart
+        mockMvc.perform(get("/add/{id}", product.getId()));
     }
 
     private MultiValueMap<String, String> getCreditCardParams() {
