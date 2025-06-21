dcf8c4708f1c38bdd01df9cb7fadd3986c234128
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-06-21 19:54:19 +0300

    adding openapi definition annotation

--- Shopping/src/main/java/com/example/shopping/ShoppingApplication.java
+++ Shopping/src/main/java/com/example/shopping/ShoppingApplication.java
@@ -1,5 +1,7 @@
 package com.example.shopping;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableCaching
 @EnableAsync
+@OpenAPIDefinition(info = @Info(title = "Products API", version = "1.0", description = "Product Microservice"))
 public class ShoppingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApplication.class, args);
